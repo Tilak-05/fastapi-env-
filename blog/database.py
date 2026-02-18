970f1fa553be9e480c2fc8f7e31f81fa540e8818
Tilak-05 <tilakluniya@gmail.com>
2026-02-18 10:56:38 +0530

    Initial FastAPI setup with virtual environment

---
+++ blog/database.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'blog.db')}"
+
+engine=create_engine(SQLALCHEMY_DATABASE_URL ,connect_args={"check_same_thread": False})
+
+SessionLocal=sessionmaker(autocommit=False,bind=engine,autoflush=False)
+Base=declarative_base()
+
+
+
---
+++ blog/main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, Depends , status ,Response , HTTPException
+from . import schema, models
+from .database import engine , SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def home():
+    return {"message": "API is running"}
+
+
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
+def create(request: schema.Blog , db : Session = Depends(get_db)):
+    new_blog=models.Blog(title=request.title , body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id , db : Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id ).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id , db : Session = Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id == id)
+
+
+@app.get("/blog")
+def all(db: Session = Depends(get_db)):
+    blog=db.query(models.Blog).all()
+    return blog
+
+@app.get('/blog/{id}', status_code=200)
+def show(id , response: Response ,db:Session = Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")        
+        #response.status_code=status.HTTP_404_NOT_FOUND
+        #return {"details": f"Blog with id {id} is not available"}   
+    return blog
+
+
